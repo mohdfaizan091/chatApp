d891b726dd8a4e49301b504008b3a3f14a4aa99a
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-02-07 20:55:43 +0530

    emiting the event and broadcasting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import {useEffect} from "react";
 import { io } from "socket.io-client";
 
 function App() {  
-  const socket = io("http://localhost:3000")
+  const socket = io("http://localhost:3000");
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected-id", socket.id);
+    });
+
+    socket.on("welcome", (msg) => {
+      console.log(msg)
+    })
+  }, []);
   return (
     <>
       <h1>hello world</h1>
--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
     console.log("user is connected");
     console.log("user id - ", socket.id);
+    socket.broadcast.emit("welcome", "welcome to the server");
 })
 
 module.exports = server;
